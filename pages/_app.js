f4b3cf145cb04515d626b720cc4e2bbefb1d4c29
Zack <Home@MacBook-Air.local>
2021-08-20 16:13:01 -0400

    update app,js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/index.css';
-import "tailwindcss/tailwind.css"
+import "tailwindcss/tailwind.css";
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
